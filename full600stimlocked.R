b7914e1f1d7c971ba55c7b78e18507e73f544d9f
Martyna Płomecka <mbplo@meth-pc01-20.dynage.local>
2020-11-02 00:30:24 +0100

    baseline corrected+stim locked

--- full600stimlocked.R
+++ full600stimlocked.R
@@ -2,12 +2,12 @@
 library(lme4)
 library(rlist)
 library(ggplot2)
-data <- read.csv('myData.csv')
+data <- read.csv('stimlockedmyData.csv')
 data$cond = as.factor(data$cond)
 data$age = as.factor(data$age)
 model = list()
 for (i in 1:600)
-{#300 ms before and 100 after the saccade
+{#300 ms before and 100 after the stimulus
   model[[i]] <- eval(parse(text=paste("lmer(X",i," ~ 1 + cond*age+ (1|Var1), data=data)", sep="")))
   summary(model[[i]])
 }
@@ -22,8 +22,7 @@ for (i in 1:600){
   print(allsummary[[i]])
 }
 
-list.save(allsummary, 'lmerallsummaries.rds')
-list.save(allsummary, 'lmerallsummaries.rdata')
+
 
 
 ### bootstrap to get reliable and stable estimates
@@ -38,27 +37,27 @@ cond = data.frame(A=numeric(0),B=numeric(0))
 for (i in 1:600){
   cond[i,] = boot[[i]][4,]
 }
-write.csv(cond,'cond.csv')
+write.csv(cond,'scond.csv')
 
 ####AGE
 age = data.frame(A=numeric(0),B=numeric(0))
 for (i in 1:600){
   age[i,] = boot[[i]][5,]
 }
-write.csv(age,'age.csv')
+write.csv(age,'sage.csv')
 
 inter = data.frame(A=numeric(0),B=numeric(0))
 for (i in 1:600){
   inter[i,] = boot[[i]][6,]
 }
-write.csv(inter,'inter.csv')
+write.csv(inter,'sinter.csv')
 ###########################################
 
 
 #############
 #############
 #############
-age <- read.csv('age.csv')
+age <- read.csv('sage.csv')
 
 names(age) <- c("X", "A", "A")
 age_binned <- rbind(age[, c(1,2)], age[, c(1,3)])
@@ -69,7 +68,7 @@ a = ggplot(age_binned, aes(X, A)) +
   scale_x_continuous(name="time points", limits=c(0, 600)) +
   scale_y_continuous(name="confidence interval", limits=c(-1.5, 2)) +
   geom_hline(yintercept=0, linetype="dashed", color = "red")+
-  ggtitle("Fixed effect age") +
+  ggtitle("stim locked Fixed effect age") +
   geom_vline(xintercept=300)+
   
   
@@ -79,7 +78,7 @@ a = ggplot(age_binned, aes(X, A)) +
 #############
 #############
 #############
-cond <- read.csv('cond.csv')
+cond <- read.csv('scond.csv')
 
 names(cond) <- c("X", "A", "A")
 cond_binned <- rbind(cond[, c(1,2)], cond[, c(1,3)])
@@ -92,7 +91,7 @@ c = ggplot(cond_binned, aes(X, A)) +
   geom_hline(yintercept=0, linetype="dashed", color = "red")+
   geom_vline(xintercept=300)+
   
-  ggtitle("Fixed effect cond (pro/anti)") +
+  ggtitle("stim locked Fixed effect cond (pro/anti)") +
   
   
   theme_light()
@@ -102,7 +101,7 @@ c = ggplot(cond_binned, aes(X, A)) +
 ###############################
 ###############################
 
-inter <- read.csv('inter.csv')
+sinter <- read.csv('sinter.csv')
 
 names(inter) <- c("X", "A", "A")
 inter_binned <- rbind(inter[, c(1,2)], inter[, c(1,3)])
@@ -112,10 +111,10 @@ i = ggplot(inter_binned, aes(X, A)) +
   geom_line(aes(group = X), alpha = 0.15) +
   scale_x_continuous(name="time points", limits=c(0, 600)) +
   scale_y_continuous(name="confidence interval", limits=c(-1.5, 1.5)) +
-   geom_hline(yintercept=0, linetype="dashed", color = "red")+
+  geom_hline(yintercept=0, linetype="dashed", color = "red")+
   geom_vline(xintercept=300)+
   
-  ggtitle("Interaction age*cond") +
+  ggtitle("stim locked Interaction age*cond") +
   theme_light()
 
 
